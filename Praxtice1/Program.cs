c6212215212928b75098c9ab29dda75e7e00dfa5
dell <dell@DESKTOP-N3F8MV8>
2024-11-15 09:24:54 +0545

    Add project files.

---
+++ Praxtice1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Practice1
+{
+    class Program
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        static void Main(string[] args)
+        {
+            Program programInstance = new Program();
+            string operation = "";
+
+            while (operation.ToUpper() != "N")
+            {
+                Console.Write("Type first number: ");
+                string input1 = Console.ReadLine();
+                Console.Write("Type second number: ");
+                string input2 = Console.ReadLine();
+
+                int firstNum;
+                int secondNum;
+
+                int.TryParse(input1, out firstNum);
+                int.TryParse(input2, out secondNum);
+
+                Console.Write("Type A for add, S for subtract, and N to exit: ");
+                operation = Console.ReadLine();
+
+                if (operation.ToUpper() == "A")
+                {
+                    int sum = programInstance.add(firstNum, secondNum);
+                    Console.WriteLine($"The sum is: {sum}");
+                }
+                else if (operation.ToUpper() == "S")
+                {
+                    int difference = programInstance.subtract(firstNum, secondNum);
+                    Console.WriteLine($"The difference is: {difference}");
+                }
+                else if (operation.ToUpper() == "N")
+                {
+                    Console.WriteLine("Exiting program.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation. Please type 'A' for add, 'S' for subtract, or 'N' to exit.");
+                }
+
+                Console.WriteLine();  
+            }
+        }
+    }
+}
